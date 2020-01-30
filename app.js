278e9ab971877e0c01689cefd9c9526032000865
John Paguirigan <johnjaroldxvi@gmail.com>
2020-01-30 11:08:07 -0700

    added hamburger menu

---
+++ app.js
@@ -0,0 +1,13 @@
+const hamburger = document.querySelector('.hamburger')
+const nav = document.querySelector('.nav-links')
+
+
+const scrollLeft = () => {
+    hamburger.addEventListener('click', () => {
+    nav.classList.toggle('active')
+    })
+
+}
+
+
+scrollLeft();
